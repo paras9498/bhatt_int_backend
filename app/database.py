17f69b3750d3b683f920e19c594935de8765e521
Pankti Jakharia <panktijakharia123@gmail.com>
2025-10-30 12:32:32 +0530

    restart

--- app/database.py
+++ app/database.py
@@ -26,7 +26,9 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+    
     try:
         yield db
     finally:
         db.close()
+        
--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from app.routers import auth, inbond, material, section, exbond, duty_space, customer, dispatch
-from app.database import engine, Base
+from app.database import engine, Base,SessionLocal
+from app.models.auth_model import User
+from sqlalchemy.exc import IntegrityError
+from datetime import datetime
+from passlib.context import CryptContext
 
 Base.metadata.create_all(bind=engine)
 
@@ -30,6 +34,35 @@ app.include_router(exbond.router)
 app.include_router(duty_space.router)
 app.include_router(customer.router)
 app.include_router(dispatch.router)
+
+# ---------- Create static admin user on startup ----------
+def create_admin_user():
+    db = SessionLocal()
+    try:
+        admin_email = "admin1@gmail.com"
+        password="admin123"
+        hashed_password = CryptContext(schemes=["bcrypt"], deprecated="auto").hash(password)
+        existing_admin = db.query(User).filter(User.email == admin_email).first()
+        if not existing_admin:
+            admin = User(
+                username="admin",
+                email=admin_email,
+                password=hashed_password,  # ⚠️ Hash this in production
+            )
+            db.add(admin)
+            db.commit()
+        else:
+            print("ℹ️ Admin user already exists.")
+    except IntegrityError:
+        db.rollback()
+        print("⚠️ Failed to insert admin user due to IntegrityError.")
+    finally:
+        db.close()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_admin_user()
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
